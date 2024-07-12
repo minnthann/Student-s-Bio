5d31ea8badb84b42ee3cdc60247e03293b85f05c
minnthann <panyobhasa93@gmail.com>
2024-07-12 22:38:58 +0630

    scendy

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,27 +6,31 @@ const shapeArr = [
     "25% 0%, 100% 0%, 75% 100%, 0% 100%",
 
     // paralleogon
-    "25% 0%, 100% 0%, 75% 100%, 0% 100%",
+    "50% 0%, 90% 20%, 100% 60%, 75% 100%, 25% 100%, 0% 60%, 10% 20%",
     // left point
     "40% 0%, 40% 20%, 100% 20%, 100% 80%, 40% 80%, 40% 100%, 0% 50%",
 
     // frame
-    "0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%",
+    "20% 0%, 0% 20%, 30% 50%, 0% 80%, 20% 100%, 50% 70%, 80% 100%, 100% 80%, 70% 50%, 100% 20%, 80% 0%, 50% 30%",
+    "20% 0%, 100% 1%, 11% 53%, 100% 80%, 80% 100%, 20% 100%, 1% 78%, 2% 21%",
+
+    "0% 15%, 15% 15%, 15% 0%, 85% 0%, 85% 15%, 100% 15%, 100% 85%, 85% 85%, 85% 100%, 15% 100%, 15% 85%, 0% 85%",
 ];
 
 
 function changeShape () {
-    const randomShape=Math.floor(Math.random()*shapeArr.length);
+    const randomShape=Math.floor(Math.random() * shapeArr.length);
     const newClipPath = shapeArr[randomShape];
     const currentClipPath = getComputedStyle(img).getPropertyValue("--shape");
 
     // နမူနာကြည့်
     // img.style.setProperty("--shape," shapeArr[randomShape]);
-    if (currentClipPath !== newClipPath) { img.style.setProperty("--shap", newClipPath);
+    if (currentClipPath !== newClipPath) { 
+        img.style.setProperty("--shape", newClipPath);
 
     }else {
-    img.style.setProperty("--shape", shapeArr[randomShape + 1]);
-    console.log(shapeArr[randomShape +1]);
+         img.style.setProperty("--shape", shapeArr[randomShape + 1]);
+         console.log(shapeArr[randomShape +1]);
     }
 
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,42 @@
-# 
-##
+#Student's Bio
 
--
-        _
+Tools:
+-HTML
+    - Pseudo Class / Pseudo Element
+    - Attributes
+    - Semantic HTML
+-CSS
+    - Universal / :root celector
+    - CSS Psuedo 
+    - ComputedStyle
+-JavaSript
+    - getElementById
+    - function
+    - conditional statement
+-Google Chome
+    - browser
+    - dev tools
+-vs Code
+    - IDE
+    - Extentions
+-Font Awesome  / cdsjs
+    -o ther necessary icons
+-git 
+    -git workflow
+-GitHub
+    - github reop
+    - github issue
+    - github pages
 
-1.
+## HTML Structure
+
+## Mockup
+
+## License
+<!-- MIT မည်သူမဆို သုံးဆွဲနိင်သည် -->
+
+## Contact Me
+    - Email:
+    - GitHub:
+
+Made with icomတည့် love by ,2024
